090bc193a6bdfc6c7f6fb98a69ac88d00e638785
Yi Tseng <a86487817@gmail.com>
2016-03-30 17:32:28 +0800

    Fix typo
    krenet -> kreonet

--- provisioning/sdnip/templates/sdnip.py
+++ provisioning/sdnip/templates/sdnip.py
@@ -81,7 +81,7 @@ class SdnIpTopo(Topo):
       |   +--+---+                  +---+--+
       |      |                          |
       +      |                          |
-    Krenet   |                          |
+   Kreonet   |                          |
              |                          |
              |                          |     Amlight
           +--+---+                  +---+--+     +
