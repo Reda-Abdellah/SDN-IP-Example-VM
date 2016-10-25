e398b7227b5bc56b8bf67a502c283b31a1d7cf18
Yi Tseng <a86487817@gmail.com>
2016-10-25 12:35:29 +0800

    add execute_single_playbook script

---
+++ execute_single_playbook.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+if [ ! -f provisioning/$1 ]; then
+  echo "playbook $1 not exist!"
+  exit 1
+fi
+
+ansible-playbook --private-key=~/.vagrant.d/insecure_private_key -u vagrant -i .vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory provisioning/$1/playbook.yml
